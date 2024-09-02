1106f6ef02d1af1c0605474b0b0a07b237fda57a
kirivanCode <ivansierravega8@gmail.com>
2024-09-02 13:27:18 -0500

    Code

---
+++ app/Filament/Resources/ProductoResource/Pages/EditProducto.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Filament\Resources\ProductoResource\Pages;
+
+use App\Filament\Resources\ProductoResource;
+use Filament\Pages\Actions;
+use Filament\Resources\Pages\EditRecord;
+
+class EditProducto extends EditRecord
+{
+    protected static string $resource = ProductoResource::class;
+
+    protected function getActions(): array
+    {
+        return [
+            Actions\DeleteAction::make(),
+        ];
+    }
+}
